d44a6980235257da242eecc99f5fe2521f2a011e
Mehul Srivastava <srimehul18@gmail.com>
2026-02-09 10:42:22 +0530

    added new buttons to clear the events and add a sample events

--- assignment.js
+++ assignment.js
@@ -31,7 +31,38 @@ function addEvent() {
         li.remove()
     })
 
+
+    const clear = document.querySelector('.clear')
+    const sample = document.querySelector('.sample')
+    if (clear) clear.addEventListener('click', clearEvents)
+    if (sample) sample.addEventListener('click', addSampleEvent)
+
     r.value = ''
     d.value = ''
     des.value = ''
+}
+
+function clearEvents() {
+    ul.innerHTML = ''
+}
+
+function addSampleEvent() {
+    const sampleEvent = {
+        ET: 'Sample Event',
+        ED: '2026-02-09',
+        cat: 'Workshop',
+        des: 'This is a sample event description.'
+    }
+
+    const li = document.createElement('li')
+    li.style.width = '250px'
+
+    li.innerHTML = `
+        <span class="text">${sampleEvent.ET}</span><br>
+        <span class="date">${sampleEvent.ED}</span><br>
+        <span class="cat">${sampleEvent.cat}</span><br>
+        <span class="des">${sampleEvent.des}</span>
+        <button class="delete">X</button>
+    `
+    ul.appendChild(li)
 }
